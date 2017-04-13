60c4c22588e2bd5bbf0375b833b9359c6b4c6c46
Jon Wong <jhannwong@gmail.com>
2017-04-13 19:18:29 +0800

    Installs Google map, and 5 locations (markers)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,71 @@
 var app = app || {}
 
-app.constructerGuard = function(constructor) {
-  if (!(this instanceof constructor)) {
-    throw new Error("constructor needs to be called with 'new' keyword");
+app.mapData = {
+  map: {
+    latlng: {lat: 1.3688548, lng: 103.9487079},
+    zoom: 15
+  },
+  locations: [
+    {name: "Downtown East",
+      latlng: {lat: 1.376684, lng: 103.954890}
+    },
+    {
+      name: "Fishing Pond",
+      latlng: {lat: 1.371890, lng: 103.952218}
+    },
+    {
+      name: "Pasir Ris Subway Station",
+      latlng: {lat: 1.373435, lng: 103.949450}
+    },
+    {
+      name: "Pasir Ris Sports Centre",
+      latlng: {lat: 1.374428, lng: 103.951986}
+    },
+    {
+      name: "Pasir Ris Beach",
+      latlng: {lat: 1.383931, lng: 103.947114}
+    }
+  ]
+};
+
+app.createMarkers = function(map) {
+  var markers = [];
+  for (var i = 0; i < app.mapData.locations.length; i++) {
+    var marker = new google.maps.Marker({
+      position: app.mapData.locations[i].latlng,
+      map: map,
+      title: app.mapData.locations[i].name
+    });
+    markers.push(marker);
   }
+
+  // Use only 1 instance of InfoWindow
+  var infoWindow = new google.maps.InfoWindow({
+    content: ''
+  });
+
+  for (var i = 0; i < markers.length; i++) {
+    (function(infoWindow, marker, map) {
+      marker.addListener('click', function() {
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map, marker);
+      });
+    })(infoWindow, markers[i], map);
+  }
+
+  return markers;
 };
 
-app.AppVM = function() {
-  app.constructerGuard.call(this, app.AppVM, "app.AppVM");
+app.initMap = function() {
+  var map;
+  map = new google.maps.Map($('#map')[0]);
 
-  this.firstName = ko.observable("Bert");
-  this.lastName = ko.observable("Bertington");
+  markers = app.createMarkers(map);
+
+  // Center and zoom map to fit all markers.
+  var markerBounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < markers.length; i++) {
+    markerBounds.extend(markers[i].getPosition());
+  }
+  map.fitBounds(markerBounds);
 };
-
-ko.applyBindings(app.AppVM());
-
